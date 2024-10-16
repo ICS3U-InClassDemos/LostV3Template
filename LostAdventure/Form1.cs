ce29113e2184f432c592486260c03ac6caabc8ef
Tony Theodoropoulos <theodot@outlook.com>
2024-10-16 11:02:33 -0400

    demo with random completed.

--- LostAdventure/Form1.cs
+++ LostAdventure/Form1.cs
@@ -22,74 +22,157 @@ namespace LostAdventure
             InitializeComponent();
 
             // Display initial message and options
+            outputLabel.Text = "You are lost in a forest.";
+            option1Label.Text = "Go North";
+            option2Label.Text = "Go South";
         }
 
         private void option1Button_Click(object sender, EventArgs e)
         {
             /// Check what page we are currently on, and then flip
             /// to the page you need to go to if you selected option 1
-            
-            if (page == 1) 
-            { 
-                
-            }
-            else if (page == 2) 
-            {
-            
-            }
-            else if (page == 3) 
-            {
-            
-            }
-            else if (page == 4) 
-            {
-            
-            }
-            else if (page == 5) 
-            {
-                        
-            }
-            else if (page == 6) 
-            {
-            
-            }
-            else if (page == 7) 
-            {
-            
-            }
 
+            if (page == 1)
+            {
+                page = 2;
+            }
+            else if (page == 2)
+            {
+                page = 4;
+            }
+            else if (page == 3)
+            {
+                page = 1;
+            }
+            else if (page == 4)
+            {
+                page = 1;
+            }
+            else if (page == 5)
+            {
+                page = 6;
+            }
+            else if (page == 6)
+            {
+                page = 1;
+            }
+            else if (page == 7)
+            {
+                page = 1;
+            }
+            else if (page == 8)
+            {
+                page = 1;
+            }
 
             /// Display text and game options to screen based on the 
             /// current page
-
-            switch (page)
-            {
-                case 1:
-                    break;
-                case 2:
-                    break;
-                case 3:
-                    break;
-                case 4:
-                    break;
-                case 5:
-                    break;
-                case 6:
-                    break;
-                case 7:
-                    break;
-                case 99:
-                    break;
-                default:
-                    break;
-            }
+            DisplayPage();
+            
         }
 
         private void option2Button_Click(object sender, EventArgs e)
         {
             ///check what page we are currently on, and then flip
             ///to the page you need to go to if you selected option 2
-            
+            if (page == 1)
+            {
+                Random randGen = new Random();  
+                int randomValue = randGen.Next(1, 101);
+
+                if (randomValue > 70)
+                {
+                    page = 8;
+                }
+                else
+                {
+                    page = 3;
+                }
+            }
+            else if (page == 2)
+            {
+                page = 5;
+            }
+            else if (page == 3)
+            {
+                page = 99;
+            }
+            else if (page == 4)
+            {
+                page = 99;
+            }
+            else if (page == 5)
+            {
+                page = 7;
+            }
+            else if (page == 6)
+            {
+                page = 99;
+            }
+            else if (page == 7)
+            {
+                page = 99;
+            }
+            else if (page == 8)
+            {
+                page = 99;
+            }
+
+            DisplayPage();
+        }
+
+        public void DisplayPage()
+        {
+            switch (page)
+            {
+                case 1:
+                    outputLabel.Text = "You are lost in a forest.";
+                    option1Label.Text = "Go North";
+                    option2Label.Text = "Go South";
+                    break;
+                case 2:
+                    outputLabel.Text = "You find a lake. Take a drink?";
+                    option1Label.Text = "Yes";
+                    option2Label.Text = "No";
+                    break;
+                case 3:
+                    outputLabel.Text = "You fall in a pit and die. Play again?";
+                    option1Label.Text = "Yes";
+                    option2Label.Text = "No";
+                    break;
+                case 4:
+                    outputLabel.Text = "The water is foul. You die of cholera. Play again?";
+                    option1Label.Text = "Yes";
+                    option2Label.Text = "No";
+                    break;
+                case 5:
+                    outputLabel.Text = "You see a horse. Do you ride it?";
+                    option1Label.Text = "Yes";
+                    option2Label.Text = "No";
+                    break;
+                case 6:
+                    outputLabel.Text = "You tame the horse and ride to safety. Play again?";
+                    option1Label.Text = "Yes";
+                    option2Label.Text = "No";
+                    break;
+                case 7:
+                    outputLabel.Text = "The horse walks past and you die of loneliness. Play again?";
+                    option1Label.Text = "Yes";
+                    option2Label.Text = "No";
+                    break;
+                case 8:
+                    outputLabel.Text = "You jump over a chasm to freedom. Play again?";
+                    option1Label.Text = "Yes";
+                    option2Label.Text = "No";
+                    break;
+                case 99:
+                    outputLabel.Text = "Thank you for playing";
+                    option1Label.Text = "";
+                    option2Label.Text = "";
+                    break;
+                default:
+                    break;
+            }
         }
 
     }
